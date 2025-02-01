67633c459290df42d15d916574e56097865b01ef
mh1sourov <sourovmh11@gmail.com>
2025-02-02 02:03:13 +0600

    total price section updated

---
+++ busticketing/JS/ticket.js
@@ -0,0 +1,130 @@
+
+// let seatAvailable = document.getElementById("avalableSeat").innerText;
+// let availableSeat = parseInt(seatAvailable);
+// let seatBus = document.getElementsByClassName("seat");
+// let clickedSeatNumber = document.getElementById("numberOfSelectedSeat").innerText;
+// let selectedSeat = parseInt(clickedSeatNumber);
+// let ticketPrice = 550;
+
+// for(let seat of seatBus){
+//     seat.addEventListener("click", function(){
+//         console.log(seat.innerText);
+//         seat.classList.add("pointer-events-none")
+//        const seatSectionDisplayDiv = document.getElementById("displayedSeatSection")
+//        let li = document.createElement("li");
+//        li.innerText = seat.innerText;
+//        seatSectionDisplayDiv.appendChild(li);
+
+//        const seatClass = document.getElementById("seatClass");
+//        let p = document.createElement("p");
+//        p.innerText = `Economic`;
+//        seatClass.appendChild(p);
+
+//     //    console.log(ticketPrice, "ticket Price", typeof ticketPrice);
+
+//     let priceContainer = document.getElementById("seatPriceContainer");
+//     let price = document.createElement("p");
+//     price.innerText = ticketPrice;
+//     priceContainer.appendChild(price)
+
+
+//     seat.classList.add("bg-[#1DD100]")
+//      selectedSeat++;
+//     //  console.log(availableSeat, "type of available seat:", typeof availableSeat)
+
+//     availableSeat--;
+
+// document.getElementById("avalableSeat").innerText = availableSeat;
+//     //  console.log(selectedSeat, typeof selectedSeat)
+
+    
+//      document.getElementById("numberOfSelectedSeat").innerText = selectedSeat;
+//        if(selectedSeat === 4){
+//         disableDiv()
+//        }
+
+//        let totalPriceString = document.getElementById("bdtPrice").innerText;
+//        let totalPrice = parseInt(totalPriceString);
+    
+//        let perSeattPrice = parseInt(price.innerText)
+       
+//        totalPrice = totalPrice + perSeattPrice;
+//        console.log(totalPrice, perSeattPrice);
+//     })
+    
+
+    
+// }
+
+// function disableDiv() {
+
+//     const seats = document.getElementsByClassName("seat"); 
+//     for (let i = 0; i < seats.length; i++) {
+//         seats[i].classList.add("pointer-events-none")}
+   
+// }
+      
+    
+
+
+
+let seatAvailable = document.getElementById("avalableSeat").innerText;
+let availableSeat = parseInt(seatAvailable);
+let seatBus = document.getElementsByClassName("seat");
+let clickedSeatNumber = document.getElementById("numberOfSelectedSeat").innerText;
+let selectedSeat = parseInt(clickedSeatNumber);
+let ticketPrice = 550;
+
+for (let seat of seatBus) {
+    seat.addEventListener("click", function () {
+        console.log(seat.innerText);
+
+        seat.style.pointerEvents = "none"; // Fixing pointer-events issue
+
+        const seatSectionDisplayDiv = document.getElementById("displayedSeatSection");
+        let li = document.createElement("li");
+        li.innerText = seat.innerText;
+        seatSectionDisplayDiv.appendChild(li);
+
+        const seatClass = document.getElementById("seatClass");
+        if (!seatClass.querySelector("p")) { // Prevent multiple Economic entries
+            let p = document.createElement("p");
+            p.innerText = `Economic`;
+            seatClass.appendChild(p);
+        }
+
+        let priceContainer = document.getElementById("seatPriceContainer");
+        let price = document.createElement("p");
+        price.innerText = ticketPrice;
+        priceContainer.appendChild(price);
+
+        seat.classList.add("bg-[#1DD100]");
+
+        selectedSeat++;
+
+        if (availableSeat > 0) { // Prevent availableSeat from going negative
+            availableSeat--;
+        }
+
+        document.getElementById("avalableSeat").innerText = availableSeat;
+        document.getElementById("numberOfSelectedSeat").innerText = selectedSeat;
+
+        if (selectedSeat >= 4) { // Fixing issue with disableDiv()
+            disableDiv();
+        }
+
+        let totalPriceString = document.getElementById("bdtPrice").innerText || "0"; // Fix NaN issue
+        let totalPrice = parseInt(totalPriceString);
+        let perSeattPrice = parseInt(price.innerText);
+
+        totalPrice = totalPrice + perSeattPrice;
+        document.getElementById("bdtPrice").innerText = totalPrice; // Make sure to update the total price
+    });
+}
+
+function disableDiv() {
+    const seats = document.getElementsByClassName("seat");
+    for (let i = 0; i < seats.length; i++) {
+        seats[i].style.pointerEvents = "none"; // Using inline style for better compatibility
+    }
+}
